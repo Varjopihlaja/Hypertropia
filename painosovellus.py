8a50345eafd912aac7ac2f358d5732d90b179f2e
Varjopihlaja <54530531+Varjopihlaja@users.noreply.github.com>
2026-05-12 21:18:21 +0200

    Update painosovellus.py

--- painosovellus.py
+++ painosovellus.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 
 from supabase import create_client
 
-SUPABASE_URL = st.secrets["https://pfaassiosclxxyudkmpe.supabase.co"]
-SUPABASE_KEY = st.secrets["sb_publishable_qZkq-1BQ3x-TBgofYW5cow_kX-R-mC-"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
